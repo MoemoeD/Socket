c22c416a50c04997e987e07f2e66dc7f4f71a7dd
MoemoeD <37505963+MoemoeD@users.noreply.github.com>
2019-02-11 11:50:56 +0800

    Server

--- Server/frmServer.Designer.cs
+++ Server/frmServer.Designer.cs
@@ -28,20 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.lblIP = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.cmbSocket = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(50, 325);
+            this.txtSend.Multiline = true;
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(675, 200);
+            this.txtSend.TabIndex = 17;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(50, 100);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(800, 200);
+            this.txtMessage.TabIndex = 16;
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(240, 50);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(47, 15);
+            this.lblPort.TabIndex = 15;
+            this.lblPort.Text = "Port:";
+            // 
+            // lblIP
+            // 
+            this.lblIP.AutoSize = true;
+            this.lblIP.Location = new System.Drawing.Point(50, 50);
+            this.lblIP.Name = "lblIP";
+            this.lblIP.Size = new System.Drawing.Size(31, 15);
+            this.lblIP.TabIndex = 14;
+            this.lblIP.Text = "IP:";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(293, 47);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(60, 25);
+            this.txtPort.TabIndex = 13;
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(87, 47);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(140, 25);
+            this.txtIP.TabIndex = 12;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(750, 325);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(100, 200);
+            this.btnSend.TabIndex = 11;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(550, 42);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(120, 30);
+            this.btnDisconnect.TabIndex = 10;
+            this.btnDisconnect.Text = "停止监听";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(400, 42);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(120, 30);
+            this.btnConnect.TabIndex = 9;
+            this.btnConnect.Text = "开始监听";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            // 
+            // cmbSocket
+            // 
+            this.cmbSocket.FormattingEnabled = true;
+            this.cmbSocket.Location = new System.Drawing.Point(700, 47);
+            this.cmbSocket.Name = "cmbSocket";
+            this.cmbSocket.Size = new System.Drawing.Size(150, 23);
+            this.cmbSocket.TabIndex = 18;
             // 
             // frmServer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(882, 553);
+            this.Controls.Add(this.cmbSocket);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.lblPort);
+            this.Controls.Add(this.lblIP);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
             this.Name = "frmServer";
             this.Text = "Server";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.Label lblIP;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ComboBox cmbSocket;
     }
 }
 
